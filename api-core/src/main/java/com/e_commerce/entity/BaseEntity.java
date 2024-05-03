1607406c15fffe895e9a63a465237f22b712ab4d
namomi <xicodey@gmail.com>
2024-05-03 18:38:34 +0900

    [style] Users -> User 클래스 변경 및 코드 수정

--- api-core/src/main/java/com/e_commerce/entity/BaseEntity.java
+++ api-core/src/main/java/com/e_commerce/entity/BaseEntity.java
@@ -1,17 +1,18 @@
 package com.e_commerce.entity;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(value = {AuditingEntityListener.class})
+@EntityListeners(value = AuditingEntityListener.class)
 public class BaseEntity {
 
 	@CreatedDate
--- api-core/src/main/java/com/e_commerce/entity/User.java
+++ api-core/src/main/java/com/e_commerce/entity/User.java
@@ -1,27 +1,32 @@
 package com.e_commerce.entity;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.e_commerce.constant.Role;
 import com.e_commerce.dto.UserInfo;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Getter
-@Table(name = "User")
-public class Users extends BaseEntity{
+public class User extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "user_id")
 	private Long id;
 
 	@Column(unique = true)
@@ -36,8 +41,8 @@ public class Users extends BaseEntity{
 	@Enumerated(EnumType.STRING)
 	private Role role;
 
-	public static Users toEntity(UserInfo userInfo, PasswordEncoder passwordEncoder) {
-		return Users.builder()
+	public static User toEntity(UserInfo userInfo, PasswordEncoder passwordEncoder) {
+		return User.builder()
 			.username(userInfo.username())
 			.password(passwordEncoder.encode(userInfo.password()))
 			.email(userInfo.email())
