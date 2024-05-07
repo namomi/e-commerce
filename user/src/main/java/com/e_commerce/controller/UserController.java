ce97f8931d9870c622ef88a59e0cccea0447a1bd
namomi <xicodey@gmail.com>
2024-05-07 09:02:23 +0900

    [feat] 유저 회원 탈퇴 기능
    유저 회원 탈퇴와 테스트

--- user/src/main/java/com/e_commerce/controller/UserController.java
+++ user/src/main/java/com/e_commerce/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.e_commerce.controller;
 
-import com.e_commerce.dto.UserInfo;
-import com.e_commerce.service.UserService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.e_commerce.dto.UserInfo;
+import com.e_commerce.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +20,10 @@ public class UserController {
 	@PostMapping("/user/register")
 	public void registerUser(@RequestBody UserInfo userInfo) {
 		userService.save(userInfo);
+	}
+
+	@DeleteMapping("/user/delete{userId}")
+	public void deleteUser(@PathVariable Long userId) {
+		userService.deleteUser(userId);
 	}
 }
--- user/src/test/java/com/e_commerce/service/UserServiceTest.java
+++ user/src/test/java/com/e_commerce/service/UserServiceTest.java
@@ -1,8 +1,11 @@
 package com.e_commerce.service;
 
-import com.e_commerce.dto.UserInfo;
-import com.e_commerce.entity.User;
-import com.e_commerce.repository.UserRepository;
+import static com.e_commerce.constant.Role.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -10,41 +13,50 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static com.e_commerce.constant.Role.ADMIN;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import com.e_commerce.dto.UserInfo;
+import com.e_commerce.entity.User;
+import com.e_commerce.repository.UserRepository;
 
 class UserServiceTest {
 
+	@Mock
+	private UserRepository userRepository;
 
-    @Mock
-    private UserRepository userRepository;
+	@Mock
+	private PasswordEncoder passwordEncoder;
 
-    @Mock
-    private PasswordEncoder passwordEncoder;
+	@InjectMocks
+	private UserService userService;
 
-    @InjectMocks
-    private UserService userService;
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+	@Test
+	void registerSuccess() {
+		// given
+		UserInfo userInfo = new UserInfo("username", "password", "email@example.com", ADMIN);
+		when(userRepository.existsByUsernameOrEmail(userInfo.username(), userInfo.email())).thenReturn(false);
+		when(passwordEncoder.encode(userInfo.password())).thenReturn("encodedPassword");
 
-    @Test
-    void registerSuccess() {
-        // given
-        UserInfo userInfo = new UserInfo("username", "password", "email@example.com", ADMIN);
-        when(userRepository.existsByUsernameOrEmail(userInfo.username(), userInfo.email())).thenReturn(false);
-        when(passwordEncoder.encode(userInfo.password())).thenReturn("encodedPassword");
+		// when
+		userService.save(userInfo);
 
-        // when
-        userService.save(userInfo);
+		// then
+		verify(userRepository).save(any(User.class));
+	}
 
-        // then
-        verify(userRepository).save(any(User.class));
-    }
+	@Test
+	void deleteSuccess() throws Exception {
+		//given
+		Long userId = 1L;
+		when(userRepository.findById(userId)).thenReturn(Optional.empty());
 
+		//when
+		userService.deleteUser(userId);
 
+		//then
+		verify(userRepository).findById(userId);
+	}
 }
